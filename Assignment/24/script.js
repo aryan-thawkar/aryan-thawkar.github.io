8fd0c3cc86f1b8f53c1216528379e2387cf14d8e
aryan <aryanthawakar975@gmail.com>
2025-07-14 16:09:15 +0530

    day 24 completed

--- Assignment/24/script.js
+++ Assignment/24/script.js
@@ -8,14 +8,48 @@ function delayedCall(fn , i) {
     setTimeout(fn , (i * 1000));
 }
 
-delayedCall(sayHello, 10);
+delayedCall(sayHello, 3);
+
+
+// Implement your own version of `.map()` as a higher-order function.
+
+array = [1, 2, 3, 4, 5];
+function cube(x) {
+    return x * x * x;
+}
+
+function myMap(arr , fn) {
+    let result = [];
+    for(let i = 0; i < arr.length; i++) {
+        result.push(fn(arr[i], i, arr));
+    }
+    return result;
+}
+
+let a = myMap(array , cube);
+console.log(a);
+
+
+// Write a function that uses closures to create a counter.
+
+function createCounter() {
+    let count = 0;
+    return function() {
+        count++;
+        console.log(count);
+    }
+}
+
+let b = createCounter();
+b();
+b();
+b();
+b();
 
 
 
 
-
-
-
+// Create a function that limits the number of times a function can be called.
 
 function limit(fn , limit){
     let calltimes = 0;
